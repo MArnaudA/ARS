746096906d6a2cb6f72216f6c059c1911ea67b64
MArnaudA <99018099+MArnaudA@users.noreply.github.com>
2023-12-17 15:31:23 +0100

    Nouvelle version 17/12/2023

--- Code.R
+++ Code.R
@@ -14,11 +14,11 @@ resume <- function(graph){
   print("density : ")
   print(graph.density(graph))
   print("transitivity : ")
-  print(transitivity(g))
+  print(transitivity(graph))
 }
 
 resume(wikipedia)
-
+mode="out"
 ############################# Distribution de degrés ###########################
 
 degreeDistRepres <- function(graph){
@@ -26,10 +26,10 @@ degreeDistRepres <- function(graph){
   plot(d,type="l")
 }
 
-degreeDistRepres(g)
+degreeDistRepres(wikipedia)
 
 ############################## Modularité R ####################################
-neighbors(g1,1,mode="out")$wikiid
+
 
 mod_R <-function(g,C,B,S){
   #R = B_{in}/B_{out}
@@ -40,7 +40,7 @@ mod_R <-function(g,C,B,S){
   bout<-length(E(g)[Bid %->% Sid])
   return (bin/(bin+bout))
 }
-id_from_wikiid(16090,g)
+
 
 ################################# Modularité M #################################
 
@@ -48,8 +48,10 @@ mod_M<-function(g,C,B,S){
   #D = B union C
   #M = D_{in}/D_{out}
   D <- union(C,B)
-  din <- length(E(g)[D %--% D])
-  dout <- length(E(g)[D %--% S])
+  D_id <- sapply(D, id_from_wikiid,g)
+  Sid=sapply(S, id_from_wikiid,g)
+  din <- length(E(g)[D_id %->% D_id])
+  dout <- length(E(g)[D_id %->% Sid])
   return(din/dout)
 }
 
@@ -57,15 +59,18 @@ mod_M<-function(g,C,B,S){
 
 mod_L<-function(g,C,B,S){
   D<-union(C,B)
-  lin<-sum(sapply(D,neighbors_in,g,D))/length(D)
-  lout <-sum(sapply(B,neighbors_in,g,S))/length(B)
+  D_id = sapply(D, id_from_wikiid, g)
+  B_id = sapply(B, id_from_wikiid, g)
+  S_id = sapply(S, id_from_wikiid, g)
+  lin<-sum(sapply(D_id,neighbors_in,g,D_id))/length(D_id)
+  lout <-sum(sapply(B_id,neighbors_in,g,S_id))/length(B_id)
   return(lin/lout)
 }
-
+sum(c(1,2,3))
 
 neighbors_in<-function(n,g,E){
-  #return the numbrer of neighbor of node n in set Ein graph g
-  return(length(intersect(neighbors(g,n),E)))
+  #return the numbrer of neighbor of node n in set E in graph g
+  return(length(intersect(neighbors(g,n,mode=mode),E)))
   
 }
 ########### Fonctions de calcul de la communauté égo-centrée ###################
@@ -74,7 +79,7 @@ update <- function(n,g,C,B,S){
   # move n in S to D
   S<- S[S!=n]
   D<- union(C,B)
-  if(all(neighbors(g,id_from_wikiid(n,g),mode="out")$wikiid %in% D)){
+  if(all(neighbors(g,id_from_wikiid(n,g),mode=mode)$wikiid %in% D)){
     # add n to C
     C <- union(C,n)
     
@@ -82,12 +87,12 @@ update <- function(n,g,C,B,S){
   else{
     #add n to B
     B<-union(B,n)
-    new_s=setdiff(neighbors(g,id_from_wikiid(n,g),mode="out")$wikiid,union(D,S))
+    new_s=setdiff(neighbors(g,id_from_wikiid(n,g),mode=mode)$wikiid,union(D,S))
     if(length(new_s)>0){
       S<-union(S,new_s)
     }
     for(b in B){
-      if(all(neighbors(g,id_from_wikiid(b,g),mode="out")$wikiid %in% D)){
+      if(all(neighbors(g,id_from_wikiid(b,g),mode=mode)$wikiid %in% D)){
         B<-B[B!=b]
         C<-union(C,b)
       }
@@ -95,10 +100,7 @@ update <- function(n,g,C,B,S){
   }
   return(list(C=C,B=B,S=S))
 } 
-
-n=S[[1]]
-neighbors(g1,id_from_wikiid(426845,g1),mode="out")$wikiid # pas de voisin
-
+n=1997
 compute_quality<-function(n,g,C,B,S,mod){
   # calcule la qualité d'une communité
   # n est un sommet de S
@@ -108,16 +110,13 @@ compute_quality<-function(n,g,C,B,S,mod){
   S<-res$S
   return(mod(g,C,B,S))
 }
-target=V(g1)[[1]]$wikiid
-mod=mod_R
-g=g1
 
 local_com <- function(target,g,mod){
   #initialisation
   if(is.igraph(g) && target %in% V(g)$wikiid){
     C<-c()
     B<-c(target)
-    S<- c(V(g)[neighbors(g,id_from_wikiid(target,g),mode="out")]$wikiid)
+    S<- c(V(g)[neighbors(g,id_from_wikiid(target,g),mode=mode)]$wikiid)
     Q<-0
     new_Q<-0
     while((length(S)>0) && (new_Q>=Q)){
@@ -138,11 +137,18 @@ local_com <- function(target,g,mod){
     stop("invalid arguments")
   }
 }
-
+target=V(g1)[[10]]$wikiid
+g=largest
+mod=mod_L
 ego_partition<-function(target,g,mod){
   res<-local_com(target,g,mod)
   res_not_com<-V(g)[!(wikiid %in% res)]$wikiid
   return(list(res,res_not_com))
+  
+}
+
+id_from_wikiid<-function(wikiid,g){
+  return(which(V(g)$wikiid==wikiid))
 }
 
 ################################ Test ##########################################
@@ -152,28 +158,55 @@ ego_partition<-function(target,g,mod){
 g1<-largest_component(wikipedia)
 
 
-# Communauté égo-centrée avec sommet cible = 1
+# Communauté égo-centrée avec sommet cible = 10
+mode="out"
 
-ego<-ego_partition(V(g1)[[10]]$wikiid,g1,mod_R)
-V(g1)[[10]]$label
-ego_ids<-sapply(ego[[1]],id_from_wikiid,g1)
+V(g1)[[180]]$label # Director telephone system
+egoR<-ego_partition(V(g1)[[180]]$wikiid,g1,mod_R) # 
+egoM<-ego_partition(V(g1)[[180]]$wikiid,g1,mod_M)
+egoL<-ego_partition(V(g1)[[180]]$wikiid,g1,mod_L)
+ego_ids_R<-sapply(egoR[[1]],id_from_wikiid,g1)
+ego_ids_M<-sapply(egoM[[1]],id_from_wikiid,g1)
+ego_ids_L<-sapply(egoL[[1]],id_from_wikiid,g1)
+ego_ids_R
+ego_ids_M
+ego_ids_L
 
-plot.igraph(induced.subgraph(g1,ego_ids))
+plot.igraph(induced.subgraph(g1,ego_ids_R))
+E(g1)
+?delete_edges
 
 V(g1)[[1]]$label # Homochronous
 
-neighbors(g1,1,mode="out")
+neighbors(g1,1,mode=mode)
 
-neighbors(g1,id_from_wikiid(V(g1)[[1]]$wikiid,g),mode="out")$wikiid
+neighbors(g1,id_from_wikiid(V(g1)[[1]]$wikiid,wikipedia),mode=mode)$wikiid
 
-id_from_wikiid<-function(wikiid,g){
-  return(which(V(g)$wikiid==wikiid))
-}
 
 id_from_wikiid(V(g1)[[1]]$wikiid,g)
 E(wikipedia)[1:10]
 
 
-neighbors(g1,id_from_wikiid(59603,g),mode="out")$wikiid
+neighbors(g1,id_from_wikiid(59603,g),mode=mode)$wikiid
+
+train=g1
+test=delete_edges(train,sample(E(train),10))
+length(E(train))
+length(E(test))
+
+wiki_deletion<-function(graph,percentage){ 
+  
+  # Prend la composante connexe la plus grande et supprime un pourcentage d'arêtes parmi cette composante
+  
+  largestComp <- largest_component(graph)
+  graph_edges_deleted<-delete_edges(largestComp,sample(E(largestComp),percentage*length(E(g))))
+  graph_edges_deleted
+}
+
+degree_distribution(largest_component(wikipedia))*length(V(largest_component(wikipedia)))
+
+largest<-largest_component(wikipedia)
+V(largest)[degree(largest)==max(degree(largest,mode=mode))]$label
+?degree
 
 
